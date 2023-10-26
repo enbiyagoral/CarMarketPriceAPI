ab87d38588d8907bc77aca468f85b8261775d3f1
Enbiya Göral <100806254+enbiyagoral@users.noreply.github.com>
2023-10-26 08:38:36 +0300

    A Basic Permissons System done!

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -4,7 +4,9 @@ import {
     Body, 
     UseGuards,
     Patch,
-    Param
+    Param,
+    Get,
+    Query
  } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
@@ -15,10 +17,17 @@ import { User } from 'src/users/user.entity';
 import { ReportDto } from './dtos/report.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ApproveReportDto } from './dtos/approve-report.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
     constructor(private reportsService: ReportsService){}
+
+    @Get()
+    getEstimate(@Query() query: GetEstimateDto){
+        console.log(query);
+    };
+
 
     @Post()
     @UseGuards(AuthGuard)
@@ -32,6 +41,8 @@ export class ReportsController {
     approveReport(@Param('id') id: string, @Body() body: ApproveReportDto){
        return this.reportsService.changeApproval(id, body.approved);
     }
+
+    
 
 
 }
